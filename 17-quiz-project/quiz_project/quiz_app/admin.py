566d1163ffb833b7ad63d85b1bab1e10edee0163
Serge <sajghaa@gmail.com>
2026-01-28 20:26:57 +0200

    models update and also admin.py updated well

--- 17-quiz-project/quiz_project/quiz_app/admin.py
+++ 17-quiz-project/quiz_project/quiz_app/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Quiz, Question, Answer, Result
 
-# Register your models here.
+# Simple registration
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Result)
+
+# OR Better: Custom admin for better UI
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at']
+    search_fields = ['title']
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['text', 'quiz']
+    list_filter = ['quiz']
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['text', 'question', 'is_correct']
+    list_filter = ['question__quiz', 'is_correct']
+
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ['user', 'quiz', 'score', 'completed_at']
+    list_filter = ['quiz', 'completed_at']
