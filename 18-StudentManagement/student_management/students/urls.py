1b71b6644b3d22f9f38198ff6ddb930a54019649
Serge <sajghaa@gmail.com>
2026-02-05 23:54:50 +0200

    Now I've added the forms and also updated the views

--- 18-StudentManagement/student_management/students/urls.py
+++ 18-StudentManagement/student_management/students/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns =[
-    path('', views.student_list, name ='student_list')
+    path('', views.student_list, name ='student_list'),
+    path('create/', views.student_create, name ='student_create')
 ]
