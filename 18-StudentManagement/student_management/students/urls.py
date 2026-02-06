f0f78eae4e6f369a1f58472eae56d96fb19d1efd
Serge <sajghaa@gmail.com>
2026-02-07 01:10:48 +0200

    Updated views, url for making the students shall update his name and id

--- 18-StudentManagement/student_management/students/urls.py
+++ 18-StudentManagement/student_management/students/urls.py
@@ -3,5 +3,6 @@ from .import views
 
 urlpatterns =[
     path('', views.student_list, name ='student_list'),
-    path('create/', views.student_create, name ='student_create')
+    path('create/', views.student_create, name ='student_create'),
+    path('update/<int:pk>/', views.student_update, name='student_update'),
 ]
--- 18-StudentManagement/student_management/students/views.py
+++ 18-StudentManagement/student_management/students/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from .models import Student
 from .forms import StudentForm
 
@@ -15,4 +15,17 @@ def student_create(request):
     else:
         form =StudentForm()
 
+    return render(request, 'students/student_form.html', {'form': form})
+
+
+def student_update(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm(instance=student)
     return render(request, 'students/student_form.html', {'form': form})
