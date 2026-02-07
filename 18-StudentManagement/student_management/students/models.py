cb60b539011da54ceab3f35bb781a9b88c5c9bcf
Serge <sajghaa@gmail.com>
2026-02-07 22:24:43 +0200

    Simple Student Management app smashed just to know the  under the hood

--- 18-StudentManagement/student_management/students/models.py
+++ 18-StudentManagement/student_management/students/models.py
@@ -4,8 +4,12 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     student_id = models.CharField (
         max_length=10, 
-        validators=[MinLengthValidator(5)]
+        validators=[MinLengthValidator(5)],
+        unique=True
         )
+    email = models.EmailField()
+    gpa = models.DecimalField(max_digits=3, decimal_places=2, default=0.0)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
