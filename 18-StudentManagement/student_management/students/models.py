a2ea77f2670a0d2c69b4da3d79db2e37f460f198
Serge <sajghaa@gmail.com>
2026-02-08 16:13:06 +0200

    models  updated

--- 18-StudentManagement/student_management/students/models.py
+++ 18-StudentManagement/student_management/students/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MinLengthValidator
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 class Student(models.Model):
     name = models.CharField(max_length=100)
     student_id = models.CharField (
@@ -8,8 +8,27 @@ class Student(models.Model):
         unique=True
         )
     email = models.EmailField()
-    gpa = models.DecimalField(max_digits=3, decimal_places=2, default=0.0)
+    gpa = models.DecimalField(
+        max_digits=3, 
+        decimal_places=2, 
+        default=0.0,
+        validators=[MinValueValidator(0.0), MaxValueValidator(4.0)]
+        )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+    
+    def get_academic_status(self):
+        if self.gpa >=3.5:
+            return "Excellent"
+        
+        elif self.gpa >= 2.0:
+            return "Good"
+        
+        else:
+            return "Needs Improvement"
+        
+    @property
+    def name_with_id(self):
+        return f"{self.name} ({self.student_id})"
