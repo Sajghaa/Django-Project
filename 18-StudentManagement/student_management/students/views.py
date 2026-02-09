842c13a6ffe3558c4da83f5e186cc1796facd2ff
Serge <sajghaa@gmail.com>
2026-02-09 16:40:48 +0200

    also views.py updated as well as forms.py

--- 18-StudentManagement/student_management/students/views.py
+++ 18-StudentManagement/student_management/students/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render, redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .models import Student
 from .forms import StudentForm
-
-
 
 def home(request):
     total_students = Student.objects.count()
@@ -13,14 +11,20 @@ def home(request):
 
 def student_list(request):
     students = Student.objects.all()
-    # search functionality
+    
+    # Search functionality
     search_query = request.GET.get('search', '')
     if search_query:
         students = students.filter(name__icontains=search_query)
+    
     return render(request, 'students/student_list.html', {
-        'students':students,
+        'students': students,
         'search_query': search_query,
-        })
+    })
+
+def student_detail(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    return render(request, 'students/student_detail.html', {'student': student})
 
 def student_create(request):
     if request.method == 'POST':
@@ -30,36 +34,30 @@ def student_create(request):
             messages.success(request, 'Student created successfully!')
             return redirect('student_list')
     else:
-        form =StudentForm()
-
+        form = StudentForm()
+    
     return render(request, 'students/student_form.html', {'form': form})
-
 
 def student_update(request, pk):
     student = get_object_or_404(Student, pk=pk)
-
+    
     if request.method == 'POST':
         form = StudentForm(request.POST, instance=student)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Student updated successfully')
-            return redirect('student_list')
+            messages.success(request, 'Student updated successfully!')
+            return redirect('student_detail', pk=student.pk)
     else:
         form = StudentForm(instance=student)
+    
     return render(request, 'students/student_form.html', {'form': form})
-
 
 def student_delete(request, pk):
     student = get_object_or_404(Student, pk=pk)
-
+    
     if request.method == 'POST':
         student.delete()
         messages.success(request, 'Student deleted successfully!')
         return redirect('student_list')
     
-    return render(request, 'students/student_confirm_delete.html', {'student':student})
-
-
-def student_detail(request, pk):
-    student = get_object_or_404(Student, pk=pk)
-    return render(request, 'students/student_detail.html', {'student': student})
+    return render(request, 'students/student_confirm_delete.html', {'student': student})
