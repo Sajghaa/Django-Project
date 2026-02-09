17fe9943bf32147a5312a2e6d4f70b27f92d0e56
Serge <sajghaa@gmail.com>
2026-02-09 16:44:14 +0200

    Updated admin.py

--- 18-StudentManagement/student_management/students/admin.py
+++ 18-StudentManagement/student_management/students/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Student
 
-admin.site.register(Student)
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'student_id', 'email', 'gpa', 'created_at')
+    search_fields = ('name', 'student_id', 'email')
+    list_filter = ('created_at',)
