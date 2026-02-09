ca1f47be8022776ecc8c46a9f260d5e3a9024f8a
Serge <sajghaa@gmail.com>
2026-02-09 16:42:39 +0200

    urls.py also updated to make sure we good

--- 18-StudentManagement/student_management/students/urls.py
+++ 18-StudentManagement/student_management/students/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .import views
+from . import views
 
-urlpatterns =[
+urlpatterns = [
     path('', views.home, name='home'),
-    path('students/', views.student_list, name ='student_list'),
-    path('create/', views.student_create, name ='student_create'),
+    path('students/', views.student_list, name='student_list'),
+    path('students/create/', views.student_create, name='student_create'),
     path('students/<int:pk>/', views.student_detail, name='student_detail'),
-    path('update/<int:pk>/', views.student_update, name='student_update'),
-    path('delete/<int:pk>/', views.student_delete, name='student_delete'),
+    path('students/update/<int:pk>/', views.student_update, name='student_update'),
+    path('students/delete/<int:pk>/', views.student_delete, name='student_delete'),
 ]
