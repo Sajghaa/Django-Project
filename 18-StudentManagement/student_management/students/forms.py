5193b8594fbdedf52fa2997b4eb9bbdcdd5cdaeb
Serge <sajghaa@gmail.com>
2026-02-09 16:38:03 +0200

    Also forms.py updated with clean codes

--- 18-StudentManagement/student_management/students/forms.py
+++ 18-StudentManagement/student_management/students/forms.py
@@ -5,9 +5,9 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ['name', 'student_id', 'email', 'gpa']
-        widgets ={
-            'gpa': forms.NumberInput(attrs={'step': '0.01', 'min':'0.0','max':'4.0'}),
+        widgets = {
+            'gpa': forms.NumberInput(attrs={'step': '0.01', 'min': '0.0', 'max': '4.0'}),
         }
-        help_texts ={
+        help_texts = {
             'gpa': 'Enter GPA between 0.0 and 4.0',
         }
