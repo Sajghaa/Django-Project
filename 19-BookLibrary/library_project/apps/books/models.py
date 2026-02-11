510cfa552d16d83740b49d49314c1cd800188f8b
Serge <sajghaa@gmail.com>
2026-02-11 17:40:55 +0200

    BookLibrary project structure

--- 19-BookLibrary/library_project/apps/books/models.py
+++ 19-BookLibrary/library_project/apps/books/models.py
@@ -1,24 +1,3 @@
 from django.db import models
-from django.core.exceptions import ValidationError
 
-class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = memoryview.CharFields(max_length=255)
-    isbn = models.CharField(max_length=13, unique=True)
-
-    category = models.CharField(max_length=100, blank=True)
-    published_date = models.DateField(null=True, blank=True)
-
-    total_copies = models.PositiveIntegerField(default = 1)
-    available_copies = models.PositiveIntegerField(default=1)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def clean(self):
-
-        if self.available_copies > self.total_copies:
-            raise ValidationError(
-                "Available copies cannot exceed total copies"
-            )
-    
-    def __str__(self):
-        return f"{self.title} by {self.author}"
+# Create your models here.
--- 19-BookLibrary/library_project/apps/borrows/apps.py
+++ 19-BookLibrary/library_project/apps/borrows/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BorrowsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'borrows'
+    name = 'apps.borrows'
