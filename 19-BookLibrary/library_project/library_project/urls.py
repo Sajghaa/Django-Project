11c72b39c737faf1e51f3ad13d95476a207f1ace
Serge <sajghaa@gmail.com>
2026-02-12 18:02:17 +0200

    Now updated views.py of books

--- 19-BookLibrary/library_project/library_project/urls.py
+++ 19-BookLibrary/library_project/library_project/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("apps.books.urls")),
     path("borrows/", include("apps.borrows.urls")),
 ]
