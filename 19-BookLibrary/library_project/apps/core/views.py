345f7e32712beca425a3fe3a8413a87b38ef1746
Serge <sajghaa@gmail.com>
2026-02-14 22:31:10 +0200

    updated core views.py

--- 19-BookLibrary/library_project/apps/core/views.py
+++ 19-BookLibrary/library_project/apps/core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from apps.borrows.models import BorrowRecord
 
-# Create your views here.
+
+@login_required
+def dashboard(request):
+    borrowed_books = BorrowRecord.objects.filter(
+        user=request.user,
+        status=BorrowRecord.Status.BORROWED
+    )
+
+    return render(
+        request,
+        "core/dashboard.html",
+        {"borrowed_books": borrowed_books},
+    )
