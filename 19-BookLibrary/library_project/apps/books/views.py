fd7629c4b86b75f304cf01c67a35a0d0ca85da83
Serge <sajghaa@gmail.com>
2026-02-14 21:31:32 +0200

    updated views.py

--- 19-BookLibrary/library_project/apps/books/views.py
+++ 19-BookLibrary/library_project/apps/books/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from apps.borrows.models import BorrowRecord
 from .models import Book
 
 
 def book_list(request):
     books = Book.objects.all()
-    return render(request, "books/book_list.html", {"books": books})
+     
+    borrowed_book_ids = BorrowRecord.objects.filter(
+        user=request.user,
+        status=BorrowRecord.Status.BORROWED
+    ).values_list("book_id", flat=True)
+
+    return render(
+        request, 
+        
+        "books/book_list.html", 
+        {
+            "books": books,
+            "borrowed_book_ids":borrowed_book_ids,
+         },
+         )
