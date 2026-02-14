0393649119ec1af08a8985af48d7709e9c332009
Serge <sajghaa@gmail.com>
2026-02-14 22:51:43 +0200

    cores apps updated also and it's still in progrss

--- 19-BookLibrary/library_project/library_project/urls.py
+++ 19-BookLibrary/library_project/library_project/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("apps.books.urls")),
+    path("", include("apps.core.urls")),
     path("borrows/", include("apps.borrows.urls")),
 ]
