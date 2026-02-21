b9280c8995aa0bf9eb0a9ab8a28aab74118d2521
Serge <sajghaa@gmail.com>
2026-02-21 23:26:03 +0200

    updated book views.py

--- 19-BookLibrary/library_project/apps/books/views.py
+++ 19-BookLibrary/library_project/apps/books/views.py
@@ -4,20 +4,32 @@ from apps.borrows.models import BorrowRecord
 from .models import Book
 
 
+@login_required
 def book_list(request):
+    """
+    Display all books with borrow/return buttons based on user status.
+    """
+
     books = Book.objects.all()
-     
+
+    # Get books currently borrowed by user
     borrowed_book_ids = BorrowRecord.objects.filter(
         user=request.user,
         status=BorrowRecord.Status.BORROWED
     ).values_list("book_id", flat=True)
 
+    # Also track overdue books
+    overdue_book_ids = BorrowRecord.objects.filter(
+        user=request.user,
+        status=BorrowRecord.Status.OVERDUE
+    ).values_list("book_id", flat=True)
+
     return render(
-        request, 
-        
-        "books/book_list.html", 
+        request,
+        "books/book_list.html",
         {
             "books": books,
-            "borrowed_book_ids":borrowed_book_ids,
-         },
-         )
+            "borrowed_book_ids": borrowed_book_ids,
+            "overdue_book_ids": overdue_book_ids,
+        },
+    )
