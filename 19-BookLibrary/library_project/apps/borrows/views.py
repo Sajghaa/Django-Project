a192ac177f9bdd5ed09712e89ba6f2cf697da571
Serge <sajghaa@gmail.com>
2026-02-21 23:18:26 +0200

    views.py updated

--- 19-BookLibrary/library_project/apps/borrows/views.py
+++ 19-BookLibrary/library_project/apps/borrows/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import redirect,render
+from django.shortcuts import redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ValidationError
-from .models import BorrowRecord
+
 from .services import borrow_book, return_book
 
 
 @login_required
 def borrow_book_view(request, book_id):
+    """
+    Handle borrowing a book via the service.
+    """
     try:
         borrow_book(user=request.user, book_id=book_id)
         messages.success(request, "Book borrowed successfully.")
@@ -19,6 +22,9 @@ def borrow_book_view(request, book_id):
 
 @login_required
 def return_book_view(request, book_id):
+    """
+    Handle returning a borrowed book via the service.
+    """
     try:
         return_book(user=request.user, book_id=book_id)
         messages.success(request, "Book returned successfully.")
@@ -26,26 +32,3 @@ def return_book_view(request, book_id):
         messages.error(request, e.message)
 
     return redirect("book_list")
-
-@login_required
-def dashboard(request):
-    borrowed_records = BorrowRecord.objects.filter(
-        user=request.user,
-        status=BorrowRecord.Status.BORROWED
-    )
-
-    # Automatically update overdue status
-    for record in borrowed_records:
-        if record.is_overdue:
-            record.status = BorrowRecord.Status.OVERDUE
-            record.save()
-
-    borrowed_records = BorrowRecord.objects.filter(
-        user=request.user
-    )
-
-    return render(
-        request,
-        "core/dashboard.html",
-        {"borrowed_books": borrowed_records},
-    )
