40f5c32a5b2c5b93ccdbb34a42e5c32d97f252d6
Serge <sajghaa@gmail.com>
2026-02-21 23:32:42 +0200

    update library_project.urls

--- 19-BookLibrary/library_project/library_project/urls.py
+++ 19-BookLibrary/library_project/library_project/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("apps.books.urls")),
-    path("", include("apps.core.urls")),
+    path("admin/", admin.site.urls),
+    path("", include("apps.books.urls")),  # book list
     path("borrows/", include("apps.borrows.urls")),
+    path("", include("apps.core.urls")),  # dashboard
+
+    # Add login/logout views
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
 ]
+
