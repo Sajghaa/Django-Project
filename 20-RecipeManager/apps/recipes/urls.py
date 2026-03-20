408acc39db7b8d811ccf70de09446891c5462306
Serge <sajghaa@gmail.com>
2026-03-20 18:49:45 +0200

    Updated recipes/urls.py

--- 20-RecipeManager/apps/recipes/urls.py
+++ 20-RecipeManager/apps/recipes/urls.py
@@ -1,13 +1,12 @@
-# apps/recipes/urls.py
 from django.urls import path
 from . import views
 
-app_name = 'apps.recipes'
+app_name = 'recipes'
 
 urlpatterns = [
-    # Add your URL patterns here
-    #path('', views.index, name='index'),
-    # You can add more patterns like:
-    # path('<int:pk>/', views.detail, name='detail'),
-    # path('create/', views.create, name='create'),
+    path('', views.recipe_list, name='list'),
+    path('recipe/<int:pk>/', views.recipe_detail, name='detail'),
+    path('recipe/create/', views.recipe_create, name='create'),
+    path('recipe/<int:pk>/update/', views.recipe_update, name='update'),
+    path('recipe/<int:pk>/delete/', views.recipe_delete, name='delete'),
 ]
