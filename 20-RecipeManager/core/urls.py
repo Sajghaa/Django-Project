7cccfe315a80cfd887ebf1248c56ddb614a7af08
Serge <sajghaa@gmail.com>
2026-03-20 17:48:56 +0200

    Updated core/urls.py

--- 20-RecipeManager/core/urls.py
+++ 20-RecipeManager/core/urls.py
@@ -1,4 +1,3 @@
-# core/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -6,9 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Now we can use just the app name since it's in Python path
-    path('', include('apps.recipes.urls')),
-    path('users/', include('apps.users.urls')),
+    path('', include('apps.recipes.urls')),  
 ]
 
 if settings.DEBUG:
