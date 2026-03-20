a8411ac400f2d817f5f4bd7e8d0b5d2e09ac1587
Serge <sajghaa@gmail.com>
2026-03-20 19:10:57 +0200

    Updated users/admin.py

--- 20-RecipeManager/apps/users/admin.py
+++ 20-RecipeManager/apps/users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'skill_level', 'date_joined')
+    list_filter = ('skill_level', 'is_active', 'is_staff')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('bio', 'avatar', 'favorite_cuisines', 'skill_level')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('bio', 'avatar', 'favorite_cuisines', 'skill_level')}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
