6788a9c0f72dd293ab35098b4481f821bc4435fa
Serge <sajghaa@gmail.com>
2026-03-20 18:58:06 +0200

    updated users/urls.py

--- 20-RecipeManager/apps/users/urls.py
+++ 20-RecipeManager/apps/users/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    # Add your URL patterns here
-    #path('', views.index, name='index'),
-    # You can add more patterns like:
-    # path('<int:pk>/', views.detail, name='detail'),
-    # path('create/', views.create, name='create'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('profile/', views.profile_view, name='profile'),
+    path('profile/edit/', views.profile_edit, name='profile_edit'),
+    path('<str:username>/', views.user_detail, name='user_detail'),
 ]
