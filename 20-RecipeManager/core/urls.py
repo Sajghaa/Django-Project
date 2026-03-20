e8ca0f4d8625f34b86d2d2a61a54d0b165de53d7
Serge <sajghaa@gmail.com>
2026-03-20 19:08:52 +0200

    updated core/urls.py by including users app

--- 20-RecipeManager/core/urls.py
+++ 20-RecipeManager/core/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.recipes.urls')),  
+    path('', include('apps.recipes.urls')), 
+    path('users/', include('apps.users.urls')), 
 ]
 
 if settings.DEBUG:
