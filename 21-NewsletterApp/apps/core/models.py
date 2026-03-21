c22faa1ed7abd7e63726837a723292c667d14158
Serge <sajghaa@gmail.com>
2026-03-21 19:16:04 +0200

    Updated apps/core/models.py

--- 21-NewsletterApp/apps/core/models.py
+++ 21-NewsletterApp/apps/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+    """Abstract base model with timestamp fields"""
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+        ordering = ['-created_at']
+
+class BaseModel(TimeStampedModel):
+    """Base model with additional fields"""
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    
+    class Meta:
+        abstract = True
+    
+    def soft_delete(self):
+        self.is_deleted = True
+        self.save()
+    
+    def restore(self):
+        self.is_deleted = False
+        self.save()
