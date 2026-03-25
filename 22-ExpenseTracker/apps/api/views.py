f33934e33d9e13aab3bf17c62156c5843c76a5ef
Serge <sajghaa@gmail.com>
2026-03-25 21:16:17 +0200

    Updated apps/api/views.py

--- 22-ExpenseTracker/apps/api/views.py
+++ 22-ExpenseTracker/apps/api/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from ..expenses.models import Expense
+from ..categories.models import Category
+from ..budgets.models import Budget
+from .serializers import ExpenseSerializer, CategorySerializer, BudgetSerializer
 
-# Create your views here.
+class ExpenseViewSet(viewsets.ModelViewSet):
+    serializer_class = ExpenseSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['category', 'date', 'payment_method']
+    search_fields = ['title', 'description']
+    ordering_fields = ['date', 'amount', 'created_at']
+    
+    def get_queryset(self):
+        return Expense.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [SearchFilter]
+    search_fields = ['name']
+    
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class BudgetViewSet(viewsets.ModelViewSet):
+    serializer_class = BudgetSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        return Budget.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
