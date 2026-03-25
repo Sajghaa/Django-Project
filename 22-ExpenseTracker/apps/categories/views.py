41fa854f4583971ad672b0476e1b8d506945765e
Serge <sajghaa@gmail.com>
2026-03-25 21:19:51 +0200

    Updated apps/categories/views.py

--- 22-ExpenseTracker/apps/categories/views.py
+++ 22-ExpenseTracker/apps/categories/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.contrib import messages
+from .models import Category
+from .forms import CategoryForm
 
-# Create your views here.
+class CategoryListView(LoginRequiredMixin, ListView):
+    model = Category
+    template_name = 'categories/list.html'
+    context_object_name = 'categories'
+    
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+
+class CategoryCreateView(LoginRequiredMixin, CreateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'categories/form.html'
+    success_url = reverse_lazy('categories:list')
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        messages.success(self.request, 'Category created successfully!')
+        return super().form_valid(form)
+
+class CategoryDetailView(LoginRequiredMixin, DetailView):
+    model = Category
+    template_name = 'categories/detail.html'
+    
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+
+class CategoryUpdateView(LoginRequiredMixin, UpdateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'categories/form.html'
+    success_url = reverse_lazy('categories:list')
+    
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Category updated successfully!')
+        return super().form_valid(form)
+
+class CategoryDeleteView(LoginRequiredMixin, DeleteView):
+    model = Category
+    template_name = 'categories/confirm_delete.html'
+    success_url = reverse_lazy('categories:list')
+    
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+    
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, 'Category deleted successfully!')
+        return super().delete(request, *args, **kwargs)
