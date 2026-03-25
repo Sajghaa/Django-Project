b72e767933865cdc80f9fbc089a2acb4c82bab5e
Serge <sajghaa@gmail.com>
2026-03-25 21:52:32 +0200

    Added forms of apps/categories

---
+++ 22-ExpenseTracker/apps/categories/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Category
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name', 'description', 'icon', 'color']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'icon': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'fas fa-tag'}),
+            'color': forms.TextInput(attrs={'class': 'form-control', 'type': 'color'}),
+        }
