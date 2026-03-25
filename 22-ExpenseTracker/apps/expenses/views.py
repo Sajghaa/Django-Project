10888291c3a0dc1af28eeaee9829b38f08d6b0f0
Serge <sajghaa@gmail.com>
2026-03-25 21:19:03 +0200

    updatd apps/expenses/views.py

--- 22-ExpenseTracker/apps/expenses/views.py
+++ 22-ExpenseTracker/apps/expenses/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.shortcuts import redirect
+from .models import Expense
+from .forms import ExpenseForm
 
-# Create your views here.
+class ExpenseListView(LoginRequiredMixin, ListView):
+    model = Expense
+    template_name = 'expenses/list.html'
+    context_object_name = 'expenses'
+    paginate_by = 20
+    
+    def get_queryset(self):
+        return Expense.objects.filter(user=self.request.user).select_related('category')
+
+class ExpenseCreateView(LoginRequiredMixin, CreateView):
+    model = Expense
+    form_class = ExpenseForm
+    template_name = 'expenses/form.html'
+    success_url = reverse_lazy('expenses:list')
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        messages.success(self.request, 'Expense added successfully!')
+        return super().form_valid(form)
+
+class ExpenseDetailView(LoginRequiredMixin, DetailView):
+    model = Expense
+    template_name = 'expenses/detail.html'
+    
+    def get_queryset(self):
+        return Expense.objects.filter(user=self.request.user)
+
+class ExpenseUpdateView(LoginRequiredMixin, UpdateView):
+    model = Expense
+    form_class = ExpenseForm
+    template_name = 'expenses/form.html'
+    success_url = reverse_lazy('expenses:list')
+    
+    def get_queryset(self):
+        return Expense.objects.filter(user=self.request.user)
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Expense updated successfully!')
+        return super().form_valid(form)
+
+class ExpenseDeleteView(LoginRequiredMixin, DeleteView):
+    model = Expense
+    template_name = 'expenses/confirm_delete.html'
+    success_url = reverse_lazy('expenses:list')
+    
+    def get_queryset(self):
+        return Expense.objects.filter(user=self.request.user)
+    
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, 'Expense deleted successfully!')
+        return super().delete(request, *args, **kwargs)
