7d5fc0872055c2d1a62bf59dcdfceab7c2dec181
Serge <sajghaa@gmail.com>
2026-03-28 13:15:57 +0200

    updated adming.py

--- 23-movie_rating_project/movies/admin.py
+++ 23-movie_rating_project/movies/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Movie, Rating
 
-# Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title', 'genre', 'release_year', 'director', 'average_rating', 'total_reviews']
+    list_filter = ['genre', 'release_year']
+    search_fields = ['title', 'director', 'description']
+    prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ['created_at', 'updated_at']
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ['movie', 'user', 'rating', 'created_at']
+    list_filter = ['rating', 'created_at']
+    search_fields = ['movie__title', 'user__username']
