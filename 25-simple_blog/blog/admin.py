3745e4255155ac55918c6da163e84ee2cfed01e2
Serge <sajghaa@gmail.com>
2026-03-30 09:42:55 +0200

    updating admin.py

--- 25-simple_blog/blog/admin.py
+++ 25-simple_blog/blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Post, Category, Comment
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'category', 'status', 'views', 'created_at']
+    list_filter = ['status', 'category', 'created_at']
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ['views', 'created_at', 'updated_at']
+    date_hierarchy = 'created_at'
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'post', 'approved', 'created_at']
+    list_filter = ['approved', 'created_at']
+    search_fields = ['name', 'email', 'content']
