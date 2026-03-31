c33dcdd8e962525e20053d033b88454fccc9a2c4
Serge <sajghaa@gmail.com>
2026-03-31 16:06:40 +0200

    updated admin.py

--- 26-markdown_blog/blog/admin.py
+++ 26-markdown_blog/blog/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Category, Post, Comment
 
-# Register your models here.
+# Define CategoryAdmin first
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'created_at', 'published_at', 'views')
+    list_filter = ('status', 'created_at', 'published_at', 'category')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'published_at'
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('title', 'slug', 'author', 'category', 'status')
+        }),
+        ('Content', {
+            'fields': ('content', 'excerpt', 'featured_image')
+        }),
+        ('Metadata', {
+            'fields': ('views',),
+            'classes': ('collapse',)
+        }),
+    )
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'post', 'created_at', 'is_approved')
+    list_filter = ('is_approved', 'created_at')
+    search_fields = ('name', 'email', 'content')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(is_approved=True)
+    approve_comments.short_description = "Approve selected comments"
+
+# Register the models with their admin classes
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
