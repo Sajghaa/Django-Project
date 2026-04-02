1d49ebb9ae586718dae4c35fd8919ec036116062
Serge <sajghaa@gmail.com>
2026-04-02 14:21:29 +0200

    updated employee_crud/urls.py

--- 27-EmployeesCrud/employee_crud/urls.py
+++ 27-EmployeesCrud/employee_crud/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +12,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        path('static/<path:path>', serve, {'document_root': settings.STATIC_ROOT}),
+        path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
