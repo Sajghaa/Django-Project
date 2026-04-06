05162cf1f299a06827b0d358e56143b8a437bd2f
Serge <sajghaa@gmail.com>
2026-04-06 15:30:17 +0200

    updated models of blog app

--- 32-comment_system/blog/models.py
+++ 32-comment_system/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', kwargs={'slug': self.slug})
+    
+    def __str__(self):
+        return self.title
