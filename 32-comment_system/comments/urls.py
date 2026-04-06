96e0ccff26abb2163b874d4b2eb9a0b4e644ba94
Serge <sajghaa@gmail.com>
2026-04-07 01:57:13 +0200

    updated comments/urls.py

--- 32-comment_system/comments/urls.py
+++ 32-comment_system/comments/urls.py
@@ -5,18 +5,18 @@ app_name = 'comments'
 
 urlpatterns = [
     # Comment CRUD
-    path('add/<str:content_type>/<int:object_id>/', views.add_comment, name='add_comment'),
+    path('add/<str:app_label>/<str:model_name>/<int:object_id>/', views.add_comment, name='add_comment'),
     path('edit/<int:comment_id>/', views.edit_comment, name='edit_comment'),
     path('delete/<int:comment_id>/', views.delete_comment, name='delete_comment'),
     path('reply/<int:comment_id>/', views.add_reply, name='add_reply'),
-       path('<int:comment_id>/', views.comment_detail, name='comment_detail'),
+    path('<int:comment_id>/', views.comment_detail, name='comment_detail'),
     
     # Interactions
     path('like/<int:comment_id>/', views.like_comment, name='like_comment'),
     path('report/<int:comment_id>/', views.report_comment, name='report_comment'),
     
     # Subscriptions
-    path('subscribe/<str:content_type>/<int:object_id>/', views.subscribe_to_thread, name='subscribe_thread'),
+    path('subscribe/<str:app_label>/<str:model_name>/<int:object_id>/', views.subscribe_to_thread, name='subscribe_thread'),
     
     # Moderation
     path('moderation/', views.moderation_dashboard, name='moderation_dashboard'),
