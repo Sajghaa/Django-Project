2e2a67cdce8a62eff3e2fd226e6fc5c8583957f8
Serge <sajghaa@gmail.com>
2026-04-08 17:21:43 +0200

    added blog/permissions.py

---
+++ 33-blog_api/blog/permissions.py
@@ -0,0 +1,45 @@
+from rest_framework import permissions
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow authors of a post to edit or delete it.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        # Write permissions are only allowed to the author of the post
+        return obj.author == request.user
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow admins to create, edit, or delete.
+    """
+    def has_permission(self, request, view):
+        # Read permissions are allowed to any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        # Write permissions are only allowed to admin users
+        return request.user and request.user.is_staff
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.author == request.user
+
+class IsCommentAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Permission for comments - only author can edit/delete.
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.author == request.user
