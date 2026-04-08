ab4295ef781fcac772fff01450338eb8f97a254c
Serge <sajghaa@gmail.com>
2026-04-08 17:40:08 +0200

    updated urls.py

--- 33-blog_api/blog/urls.py
+++ 33-blog_api/blog/urls.py
@@ -3,13 +3,12 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
-# Create router for ViewSets
 router = DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'categories', views.CategoryViewSet)
-router.register(r'tags', views.TagViewSet)
-router.register(r'posts', views.PostViewSet)
-router.register(r'comments', views.CommentViewSet)
+router.register(r'users', views.UserViewSet, basename='user')
+router.register(r'categories', views.CategoryViewSet, basename='category')
+router.register(r'tags', views.TagViewSet, basename='tag')
+router.register(r'posts', views.PostViewSet, basename='post')
+router.register(r'comments', views.CommentViewSet, basename='comment')
 
 urlpatterns = [
     # API root
