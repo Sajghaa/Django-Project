c4eaf2c41c4f6d4dba462ce9e95f4accaa663e50
Serge <sajghaa@gmail.com>
2026-04-08 17:21:06 +0200

    Added new file(pagination.py)

---
+++ 33-blog_api/blog/pagination.py
@@ -0,0 +1,43 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+class CustomPagination(PageNumberPagination):
+    """Custom pagination class"""
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+    
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'current_page': self.page.number,
+            'page_size': self.page_size,
+            'results': data
+        })
+
+class PostPagination(PageNumberPagination):
+    """Custom pagination for posts"""
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 50
+    
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'current_page': self.page.number,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
+
+class CommentPagination(PageNumberPagination):
+    """Custom pagination for comments"""
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 100
