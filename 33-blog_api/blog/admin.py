871401ab2d391645d958f8746799bd781c352120
Serge <sajghaa@gmail.com>
2026-04-08 17:19:45 +0200

    Updated admin.py

--- 33-blog_api/blog/admin.py
+++ 33-blog_api/blog/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Category, Tag, Post, Comment
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'created_at']
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'category', 'status', 'views', 'likes', 'published_at']
+    list_filter = ['status', 'category', 'created_at']
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ['views', 'likes', 'created_at', 'updated_at']
+    date_hierarchy = 'published_at'
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'post', 'content', 'approved', 'created_at']
+    list_filter = ['approved', 'created_at']
+    search_fields = ['author__username', 'content']
