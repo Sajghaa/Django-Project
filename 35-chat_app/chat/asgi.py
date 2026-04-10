cd53c633fabf01975075d7a39dd53f44f14399d3
Serge <sajghaa@gmail.com>
2026-04-10 22:33:03 +0200

    Project structure of chat-app

---
+++ 35-chat_app/chat/asgi.py
@@ -0,0 +1,24 @@
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.urls import path
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
+
+# Import routing after Django setup
+django_asgi_app = get_asgi_application()
+
+from chat import routing
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                routing.websocket_urlpatterns
+            )
+        )
+    ),
+})
