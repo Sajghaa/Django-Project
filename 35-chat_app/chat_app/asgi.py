f55cecbda2591361aa254e751a0301a078d552ce
Serge <sajghaa@gmail.com>
2026-04-11 11:35:21 +0200

    I've reframed this so that I can support dapne

--- 35-chat_app/chat_app/asgi.py
+++ 35-chat_app/chat_app/asgi.py
@@ -1,16 +1,22 @@
-"""
-ASGI config for chat_app project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.urls import path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
 
-application = get_asgi_application()
+# Initialize Django ASGI application
+django_asgi_app = get_asgi_application()
+
+# Import consumers after Django setup
+from chat import consumers
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            path('ws/chat/<str:room_slug>/', consumers.ChatConsumer.as_asgi()),
+        ])
+    ),
+})
