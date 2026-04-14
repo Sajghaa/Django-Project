23e0bb210881966f85d04073e85912ad6bf5d6c7
Serge <sajghaa@gmail.com>
2026-04-14 22:07:04 +0200

    Updated admin.py

--- 36-file_app/files/admin.py
+++ 36-file_app/files/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import File, FileDownload
 
-# Register your models here.
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ['original_name', 'uploaded_by', 'file_size', 'access_type', 'download_count', 'created_at']
+    list_filter = ['access_type', 'is_active', 'created_at']
+    search_fields = ['original_name', 'title', 'uploaded_by__username']
+    readonly_fields = ['file_hash', 'download_count', 'created_at', 'updated_at']
+
+@admin.register(FileDownload)
+class FileDownloadAdmin(admin.ModelAdmin):
+    list_display = ['file', 'user', 'ip_address', 'downloaded_at']
+    list_filter = ['downloaded_at']
+    search_fields = ['file__original_name', 'user__username']
