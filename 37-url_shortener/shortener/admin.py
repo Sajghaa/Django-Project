12af5d26ee9757c991dd4aeacd2d4d89cee7fa85
Serge <sajghaa@gmail.com>
2026-04-15 15:18:06 +0200

    Updated admin.py

--- 37-url_shortener/shortener/admin.py
+++ 37-url_shortener/shortener/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import URL, Click, UserProfile
 
-# Register your models here.
+@admin.register(URL)
+class URLAdmin(admin.ModelAdmin):
+    list_display = ['short_code', 'original_url', 'user', 'total_clicks', 'is_active', 'created_at']
+    list_filter = ['is_active', 'created_at']
+    search_fields = ['short_code', 'original_url', 'user__username']
+    readonly_fields = ['total_clicks', 'unique_clicks', 'created_at', 'updated_at']
+
+@admin.register(Click)
+class ClickAdmin(admin.ModelAdmin):
+    list_display = ['url', 'ip_address', 'country', 'device_type', 'clicked_at']
+    list_filter = ['device_type', 'clicked_at']
+    search_fields = ['url__short_code', 'ip_address']
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'total_links', 'total_clicks', 'created_at']
