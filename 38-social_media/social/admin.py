67dbdf4485075c272cb14d6a0691260909cd64f8
Serge <sajghaa@gmail.com>
2026-04-16 15:34:11 +0200

    Updated admin.py

--- 38-social_media/social/admin.py
+++ 38-social_media/social/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Profile, Post, Follow, Like, Comment, Notification
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'followers_count', 'following_count', 'posts_count']
+    search_fields = ['user__username']
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['user', 'content', 'likes_count', 'comments_count', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['user__username', 'content']
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ['follower', 'following', 'created_at']
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['user', 'post', 'created_at']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'post', 'content', 'created_at']
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['user', 'from_user', 'notification_type', 'is_read', 'created_at']
+    list_filter = ['notification_type', 'is_read']
