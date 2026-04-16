7f83a52f0a43d5ccbf2a451d1946ab11860406d4
Serge <sajghaa@gmail.com>
2026-04-16 14:56:48 +0200

    Added contx

---
+++ 38-social_media/social/context_processors.py
@@ -0,0 +1,8 @@
+from .models import Notification
+
+def notification_count(request):
+    """Add unread notification count to all templates"""
+    if request.user.is_authenticated:
+        count = Notification.objects.filter(user=request.user, is_read=False).count()
+        return {'notification_count': count}
+    return {'notification_count': 0}
