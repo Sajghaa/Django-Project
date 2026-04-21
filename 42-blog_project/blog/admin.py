b41caf42f1b40589db2938fef129f498153648da
Serge <sajghaa@gmail.com>
2026-04-21 08:21:39 +0200

    Updated blog/admin.py

--- 42-blog_project/blog/admin.py
+++ 42-blog_project/blog/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Category, Tag, Post, Comment, UserProfile
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'post_count', 'created_at']
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'post_count', 'created_at']
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'category', 'status', 'views', 'likes', 'published_at']
+    list_filter = ['status', 'category', 'created_at']
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ['views', 'likes', 'created_at', 'updated_at']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'post', 'content', 'approved', 'created_at']
+    list_filter = ['approved', 'created_at']
+    search_fields = ['author__username', 'content']
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'post_count', 'created_at']
+    search_fields = ['user__username']
