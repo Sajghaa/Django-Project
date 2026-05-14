1013ec97e7496b759230cb0ca3d0c731ded05136
Serge <sajghaa@gmail.com>
2026-05-14 21:58:21 +0200

    Updated admin.py

--- 49-image_processor/images/admin.py
+++ 49-image_processor/images/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Image, ProcessedImage
 
-# Register your models here.
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['original_name', 'user', 'width', 'height', 'file_size', 'created_at']
+    list_filter = ['format', 'created_at']
+    search_fields = ['original_name', 'user__username']
+    readonly_fields = ['filename', 'file_size', 'created_at']
+
+@admin.register(ProcessedImage)
+class ProcessedImageAdmin(admin.ModelAdmin):
+    list_display = ['original_image', 'size_type', 'width', 'height', 'file_size', 'created_at']
+    list_filter = ['size_type', 'created_at']
+    search_fields = ['original_image__original_name']
