3eb78db571392660f22febd21931e23cf1ff47aa
Serge <sajghaa@gmail.com>
2026-05-20 14:32:05 +0200

    Updating admin.py

--- 51-django_react_blog/blog/admin.py
+++ 51-django_react_blog/blog/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Category, Tag, Post, Comment, Like
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'created_at']
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'created_at']
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'category', 'status', 'views', 'likes', 'created_at']
+    list_filter = ['status', 'category', 'created_at']
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ['views', 'likes', 'created_at', 'updated_at']
+    date_hierarchy = 'created_at'
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'post', 'approved', 'created_at']
+    list_filter = ['approved', 'created_at']
+    search_fields = ['author__username', 'content']
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['user', 'post', 'created_at']
+    list_filter = ['created_at']
