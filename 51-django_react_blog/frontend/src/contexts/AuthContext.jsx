aeb7f6fa0b4d54b8fe0c15b2a17b3d87cd021e79
Serge <sajghaa@gmail.com>
2026-05-21 17:38:17 +0200

    51-django_react_blog/frontend/src/contexts/AuthContext.jsx

--- 51-django_react_blog/frontend/src/contexts/AuthContext.jsx
+++ 51-django_react_blog/frontend/src/contexts/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
-import { login as apiLogin, register as apiRegister, refreshToken } from '../services/api';
+import { login as apiLogin, register as apiRegister } from '../services/api';
 import { toast } from 'react-toastify';
 
 const AuthContext = createContext();
