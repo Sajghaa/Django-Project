24974afe15f988ab7fb80699aedccf15af76d5ef
Serge <sajghaa@gmail.com>
2026-06-27 15:30:50 +0200

    Updated forms.py

--- 53-blog_like_unlike/blog/forms.py
+++ 53-blog_like_unlike/blog/forms.py
@@ -1,10 +1,45 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
+from .models import Post, Comment, UserProfile
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'category', 'tags', 'content', 'featured_image']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 10, 'class': 'rich-text-editor'}),
+            'tags': forms.SelectMultiple(attrs={'class': 'tag-select'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['category'].empty_label = 'Select Category'
+        self.fields['tags'].required = False
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write a comment...'}),
+        }
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['bio', 'avatar', 'website', 'location', 'birth_date']
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
