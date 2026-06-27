61e6c323a2bf4120da0c84a88ab59b47106cc49b
Serge <sajghaa@gmail.com>
2026-06-27 15:10:05 +0200

    Updated views.py

--- 53-blog_like_unlike/blog/views.py
+++ 53-blog_like_unlike/blog/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from .models import Post, Like
+from .forms import RegisterForm
 
-# Create your views here.
+def home(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/home.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    user_liked = False
+    if request.user.is_authenticated:
+        user_liked = Like.objects.filter(user=request.user, post=post).exists()
+    return render(request, 'blog/post_detail.html', {
+        'post': post,
+        'user_liked': user_liked,
+    })
+
+@login_required
+@require_POST
+def like_toggle(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    like, created = Like.objects.get_or_create(user=request.user, post=post)
+    if not created:
+        like.delete()
+        liked = False
+    else:
+        liked = True
+    return JsonResponse({
+        'liked': liked,
+        'likes_count': post.likes.count(),
+    })
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
