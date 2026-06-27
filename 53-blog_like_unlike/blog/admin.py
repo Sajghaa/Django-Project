980efb6f9eb3c1f8553ff4ade1a486b1e912f033
Serge <sajghaa@gmail.com>
2026-06-27 15:19:15 +0200

    Updated Admin.py

--- 53-blog_like_unlike/blog/admin.py
+++ 53-blog_like_unlike/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post, Like
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'likes_count')
+    list_filter = ('created_at', 'author')
+    search_fields = ('title', 'content')
+
+    def likes_count(self, obj):
+        return obj.likes.count()
+    likes_count.short_description = 'Likes'
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'created_at')
