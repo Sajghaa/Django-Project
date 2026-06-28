24aca5ca524ff2a30b075af5df4b7aebc35b8153
Serge <sajghaa@gmail.com>
2026-06-28 21:44:13 +0200

    updated users models

--- 54-elearning/users/models.py
+++ 54-elearning/users/models.py
@@ -1,3 +1,12 @@
+# apps/users/models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    is_instructor = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=True)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True)
+    
+    def __str__(self):
+        return self.email
